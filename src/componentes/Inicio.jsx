c124dac5df7a28bef384eec0dc195792526d30ac
TomasRivarola451 <tomasrivarola451@gmail.com>
2025-10-28 19:52:19 -0300

    fix: corregir importación de Inicio.css

--- src/componentes/Inicio.jsx
+++ src/componentes/Inicio.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from "react-router-dom";
-import './Inicio.css';
+import './inicio.css';
 import HeroSlider from './HeroSlider';
 
 export default function Inicio() {
